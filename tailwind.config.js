19973ada9ee177547b26c73c410c145cf8b103e6
zain sheikh <zaynsheikh881@gmail.com>
2025-01-20 21:43:49 +0500

    create home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "primary-color": "#dd0000",
+        "secondary-color": "#fecd00",
+      },
+    },
   },
   plugins: [],
 };
